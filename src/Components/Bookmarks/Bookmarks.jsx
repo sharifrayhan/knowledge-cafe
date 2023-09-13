9e10381484692fdca0e33151e1b341dd415fc8f4
Your Name <you@example.com>
2023-09-13 21:50:49 +0600

    second commit

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,10 @@
 
 
-const Bookmarks = () => {
+const Bookmarks = ({newBookmarks}) => {
+    const [blog_title] = newBookmarks;
     return (
         <div >
-            <h1>Bookmarks</h1>
+            <h1 className="font-bold">Bookmarked Blogs{newBookmarks.length}</h1>
         </div>
     );
 };
